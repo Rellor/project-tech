e84b3c5be02f073e1460370cbdca428c5fcf880c
Rellor <basderoller@gmail.com>
2021-04-20 15:15:35 +0200

    EJS Implementation and route tests

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 6969
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World! dit is een test! Cool he!!!!!')
+  res.render('index',  { title: 'Travel Buddy | Home', message: 'Welkom!!', kutzooi: 'Ja, kutzooi' })
 })
 
 app.get('/About', (req, res) => {
   const name = 'Danny'
   res.send('Hello ${name}')
+})
+
+// 404 pagina
+app.use((req, res, next) => {
+    res.send('404 Deze pagina bestaat niet!')
 })
 
 app.listen(port, () => {
