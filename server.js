346bf236a050c1db9a66f1cde2767331f5f4e6a7
Rellor <basderoller@gmail.com>
2021-04-23 17:10:44 +0200

    Added form

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
 const port = 6969
 
-app.use(express.static('static'))
+app.use(express.static('static'));
+app.use(bodyParser());
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
   res.render('index',  { title: 'Dark Souls 3 Matcher', homebutton:'login', homebuttonlink:'Tap screen', kutzooilink: '', kutzooi: '' })
 })
+
+app.get('/login', (req, res) => {
+  res.render('pages/login',  { title: 'Login'})
+  res.sendfile("login");
+})
+
+app.post('/login', (req, res) => {
+  var naam = req.body.firstname;
+  console.log(naam);
+    res.render('pages/login',  { title: 'Login', naam: naam})
+})
+
+app.post('/login', function(req, res, next){
+   // req.body object has your form values
+   console.log(req.body.firstname);
+});
 
 // 404 pagina
 app.use((req, res, next) => {
