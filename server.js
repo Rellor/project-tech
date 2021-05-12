ac455ef63bdc28fb55907b98c4daa6006efd071c
Rellor <basderoller@gmail.com>
2021-05-12 12:32:57 +0200

    port update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 6969;
-
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
