0c9822208ab7b5981d14e5a5838ce602f42e6dda
Rellor <basderoller@gmail.com>
2021-05-14 12:10:14 +0200

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 6969
 
 require('dotenv').config({
   path: '.env'
